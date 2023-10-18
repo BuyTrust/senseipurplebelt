56a312b27a9fa843c3064ec958da65cd928f8952
JF <107821182+BuyTrust@users.noreply.github.com>
2023-10-18 16:25:46 -0400

    Finish SuperShapes
    ahrj45thr4nhhjt3jukijuhygtrtkujyrfhjukifrdekifrki

--- Super Shapes/Assets/Scripts/GameController.cs
+++ Super Shapes/Assets/Scripts/GameController.cs
@@ -6,14 +6,29 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("Shape objects")] public GameObject[] shapePrefabs;
+    [Header("Default spawn delay time")] public float spawnDelay = 2f;
+    [Header("Default spawn time")] public float spawnTime = 3f;
+    [Header("game ove rui object")] public GameObject gameOverCanvas;
     void Start()
     {
-
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void Spawn()
+    {
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        CancelInvoke("Spawn");
+        Time.timeScale = 0;
     }
 }
--- Super Shapes/Assets/Scripts/Shape.cs
+++ Super Shapes/Assets/Scripts/Shape.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Shape : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("Rigidbody obj")] public Rigidbody2D rb;
+    [Header("Default shrinking speed")] public float shrinkSpeed = 3f;
+
     void Start()
     {
-
+        rb.rotation = Random.Range(0f, 360f);
+        transform.localScale = Vector3.one * 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+        if (transform.localScale.x <= .05f)
+        {
+            Destroy(gameObject);
+            Score.score++;
+        }
     }
 }
